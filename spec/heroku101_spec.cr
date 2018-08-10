1103006df07300fd351877c3bdd05ac453ad0836
Colton Loewen <loewencjay@gmail.com>
2018-08-10 00:44:01 -0600

    Spec now passes
    Although the initial project has a test that always fails, using this
    with Heroku's "Wait for CI to pass before deploy" option could lead to
    the initial project never deploying.

--- spec/heroku101_spec.cr
+++ spec/heroku101_spec.cr
@@ -4,6 +4,6 @@ describe "Heroku101" do
   # TODO: Write tests
 
   it "works" do
-    false.should eq(true)
+    true.should eq(true)
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/heroku101"
+require "../src/heroku101/*"
